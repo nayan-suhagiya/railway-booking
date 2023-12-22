b7f55281f81358713e2d7b8d5ad6305074157c86
nayan-suhagiya <suhagiya.nayan01@gmail.com>
2023-12-22 11:29:26 +0530

    ..

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -12,10 +12,13 @@ import { StationInterface } from 'src/app/models/Station';
 })
 export class SearchComponent implements OnInit {
   departureStationId!: number;
+  departureStation!: string;
   arrivalStationId!: number;
+  arrivalStation!: string;
   departureDate!: string;
   searchData!: SearchModel;
   searchResult!: SearchModel[];
+  searchResultCount: number = 0;
   stationList!: StationInterface[];
 
   constructor(
@@ -48,7 +51,8 @@ export class SearchComponent implements OnInit {
     this.stationService.getTrainsBetweenStations(this.searchData).subscribe({
       next: (res: ResponseInterface) => {
         this.searchResult = res.data;
-        console.log(this.searchResult);
+        this.searchResultCount = res.data.length;
+        // console.log(this.searchResult);
       },
       error: (err) => {
         console.log(err);
@@ -64,6 +68,25 @@ export class SearchComponent implements OnInit {
       next: (res: ResponseInterface) => {
         this.stationList = res.data;
         // console.log(this.stationList);
+
+        const departureStationData = this.stationList.find((station) => {
+          return station.stationID == this.departureStationId;
+        });
+        // console.log(departureStationData);
+        this.departureStation = String(departureStationData?.stationName);
+
+        const arrivalStationData = this.stationList.find((station) => {
+          return station.stationID == this.arrivalStationId;
+        });
+        // console.log(arrivalStationData);
+        this.arrivalStation = String(arrivalStationData?.stationName);
+
+        this.stationList = this.stationList.filter((station) => {
+          return (
+            station.stationID != this.departureStationId &&
+            station.stationID != this.arrivalStationId
+          );
+        });
       },
       error: (err) => {
         console.log(err);
